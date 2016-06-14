38f7d36b015126ce420825b5b3c356407ab4d1b5
raul <valls.raul@gmail.com>
2016-06-14 20:56:14 +0200

    Initial commit

---
+++ app/src/main/java/es/rul3s/firebasetest/MainActivity.java
@@ -0,0 +1,16 @@
+package es.rul3s.firebasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().subscribeToTopic("test");
+    }
+}
