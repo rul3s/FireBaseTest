3aa92c30472de2f6087db0ee3e0fe12d1a66555d
raul <valls.raul@gmail.com>
2016-06-15 10:47:49 +0200

    Added FireBase Messaging · JSON · Graddle.build · MessagingSvc · AndroidManifest Service

--- app/src/main/java/es/rul3s/firebasetest/MainActivity.java
+++ app/src/main/java/es/rul3s/firebasetest/MainActivity.java
@@ -1,16 +1,31 @@
 package es.rul3s.firebasetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+    public Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        //
+    }
+
+    public void subscribe(View v){
+        Log.d("INFO:","Subscribe button");
         FirebaseMessaging.getInstance().subscribeToTopic("test");
+    }
+
+    public void unsubscribe(View v){
+        Log.d("INFO:","Unsubscribe button");
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("test");
     }
 }
